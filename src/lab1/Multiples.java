7fa6be9c63812273104d1a2ffb1e2e6fd79933cd
Rocky Shi <115847633+RockyS-07@users.noreply.github.com>
2026-09-15 15:43:57 -0400

    fix Multiples.java so it is strictly < n

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,10 +2,14 @@ package lab1;
 
 public class Multiples {
     static void main() {
+        System.out.println(multiples(1000, 3, 5));
+    }
+
+    static int multiples(int n, int a, int b) {
         int ans = 0;
-        for (int i = 1; i <= 1000; i++) {
-            if (i % 3 == 0 || i % 5 == 0) ans++;
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) ans++;
         }
-        System.out.println(ans);
+        return ans;
     }
 }
