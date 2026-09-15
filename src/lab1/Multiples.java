9f60991242ed905fede424d58e9660249768c625
Rocky Shi <115847633+RockyS-07@users.noreply.github.com>
2026-09-15 14:39:24 -0400

    solution to Extra Task 1

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class Multiples {
+    static void main() {
+        int ans = 0;
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) ans++;
+        }
+        System.out.println(ans);
+    }
+}
